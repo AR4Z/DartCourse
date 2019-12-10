6c35d1fae6d4ceae690ddfe5432073a920fed7f1
Orlando Hernandez <oaraz02@gmail.com>
2019-12-09 20:23:00 -0500

    add strings

---
+++ Data Types/strings.dart
@@ -0,0 +1,10 @@
+main() {
+  String nombre = 'Tony';
+  String nombre2 = 'O\'Connor';
+  String multilinea = '''1 linea
+                        2 lineas
+                        3 lineas''';
+  String multilinea2 = """1linea
+                          2lineas
+                          3lineas""";
+}
